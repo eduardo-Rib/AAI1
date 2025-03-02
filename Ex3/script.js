93f4eb5046964045646a4132d4fb775400d0d850
Eduardo Fonseca Ribeiro <eduardo10122005@gmail.com>
2025-03-02 19:21:30 -0300

    Add ex3

--- Ex3/script.js
+++ Ex3/script.js
@@ -1,24 +1,30 @@
-function calcularFaixaEtaria() {
-    let idade = document.getElementById('idade').value.trim(); 
+function calcularMedia() {
+    let nota1 = parseFloat(document.getElementById("nota1").value);
+    let nota2 = parseFloat(document.getElementById("nota2").value);
+    let nota3 = parseFloat(document.getElementById("nota3").value);
 
-    try {
-       if (!idade) {
-            throw new Error("Insira um valor!");
-        }
-
-        let classificacao;
-        if ((idade >= 0)&&(idade < 15)) {
-            classificacao = 'criança'
-        } else if ((idade >= 15)&&(idade < 30)) {
-            classificacao = 'jovem'
-        } else if ((idade >= 30)&&(idade < 60)) {
-            classificacao = 'adulto'
-        } else {
-            classificacao = 'idoso'
-        }
-        
-        document.getElementById("resultado").innerText = `${idade} é classificado como: ${classificacao}.`
-    } catch (error) {
-        document.getElementById("resultado").innerText = error.message
+    if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3)) {
+        document.getElementById("resultado").innerText = "Por favor, preencha todas as notas!";
+        return;
     }
+
+    let media = nota1 * 0.2 + nota2 * 0.5 + nota3 * 0.3;
+    let classificacao = "";
+
+    if (media >= 9) {
+        classificacao = "A";
+    } else if (media >= 8) {
+        classificacao = "B";
+    } else if (media >= 7) {
+        classificacao = "C";
+    } else if (media >= 6) {
+        classificacao = "D";
+    } else if (media >= 5) {
+        classificacao = "E";
+    } else {
+        classificacao = "F";
+    }
+
+    document.getElementById("resultado").innerText = 
+        `A média do aluno é = ${media.toFixed(2)} e a sua classificação é ${classificacao}`;
 }
